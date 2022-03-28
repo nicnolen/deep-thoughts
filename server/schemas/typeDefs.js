609152e32a1f8b60f7b76eeb7f531990bfa0edb2
Nicholas Nolen <nicnolen@ymail.com>
2022-03-28 14:08:09 -0500

    added a username parameter to the thoughts Query in the typeDefs.js file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,16 +6,19 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   """ create a thoughts query that returns _id, thoughtText, username, and reactionCount fields and their GraphQL scalars(datatypes) """
   type Thought {
-    _id: ID # ... same as String but looking for a unique identifier
+    # ... same as String but looking for a unique identifier
+    _id: ID 
     thoughtText: String
     createdAt: String
     username: String
-    reactionCount: Int # ... integer
+    # ... integer
+    reactionCount: Int 
   }
 
   """ query through data and return an array of the Thought data type """
   type Query {
-    thoughts: [Thought]
+    # could recieve a parameter (username with a String datatype) if we wanted to 
+    thoughts(username: String): [Thought]
   }
 `; // This is a tagged template function, used to provide explicit details on how a library is used
 
