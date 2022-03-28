cb76c477f501d80ab5c044c12517854736872512
Nicholas Nolen <nicnolen@ymail.com>
2022-03-28 14:27:56 -0500

    added a resolver to look up thoughts by username

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,15 @@
 /* CREATE RESOLVERS TO PERFORM CRUD ACTIONS ON QUERIES AND MUTATIONS */
 // Import User and Thought models
-const { User, Thought } = require('../models')
+const { User, Thought } = require('../models');
+
 // Create resolvers
 const resolvers = {
   // Thoughts query resolver
   Query: {
-    thoughts: async() => {
-      return Thought.find().sort({ createdAt: -1 });
-    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {}; // ternary operator to check if username exists
+      return Thought.find(params).sort({ createdAt: -1 });
+    }, // parent used as a placeholder so we can access the username argument from the second parameter
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,21 +4,22 @@ const { gql } = require('apollo-server-express');
 
 // Create our type definitions (typeDefs)
 const typeDefs = gql`
-  """ create a thoughts query that returns _id, thoughtText, username, and reactionCount fields and their GraphQL scalars(datatypes) """
+  """
+   create a thoughts query that returns _id, thoughtText, username, and reactionCount fields and their GraphQL scalars(datatypes)
+  """
   type Thought {
-    # ... same as String but looking for a unique identifier
-    _id: ID 
+    _id: ID # ... same as String but looking for a unique identifier
     thoughtText: String
     createdAt: String
     username: String
-    # ... integer
-    reactionCount: Int 
+    reactionCount: Int # ... integer
   }
 
-  """ query through data and return an array of the Thought data type """
+  """
+   query through data and return an array of the Thought data type
+  """
   type Query {
-    # could recieve a parameter (username with a String datatype) if we wanted to 
-    thoughts(username: String): [Thought]
+    thoughts(username: String): [Thought] # ... could recieve a parameter (username with a String datatype) if we wanted to
   }
 `; // This is a tagged template function, used to provide explicit details on how a library is used
 
