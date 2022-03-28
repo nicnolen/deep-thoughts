9e6a8835ea4bccc63ec772db62b5c27523d2e11c
Nicholas Nolen <nicnolen@ymail.com>
2022-03-28 14:38:38 -0500

    updated Thought query to include Reaction query as an array

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,7 @@ const { gql } = require('apollo-server-express');
 // Create our type definitions (typeDefs)
 const typeDefs = gql`
   """
-   create a Thought query that returns _id, thoughtText, username, and reactionCount fields and their GraphQL scalars(datatypes)
+  create a Thought query that returns _id, thoughtText, username, and reactionCount fields and their GraphQL scalars(datatypes)
   """
   type Thought {
     _id: ID # ... same as String but looking for a unique identifier
@@ -13,18 +13,21 @@ const typeDefs = gql`
     createdAt: String
     username: String
     reactionCount: Int # ... integer
+    reactions: [Reaction]
   }
 
   """
   create a Reaction query that returns _id, reactionBody, createdAt, and username
   """
   type Reaction {
-  _id: ID
-  reactionBody: String
-  createdAt: String
-  username: String
-  
-   query through data and return an array of the Thought data type
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  """
+  query through data and return an array of the Thought data type
   """
   type Query {
     thoughts(username: String): [Thought] # ... could recieve a parameter (username with a String datatype) if we wanted to
