490ec796e42a2a1c29b7e258385894eb128d6dde
Nicholas Nolen <nicnolen@ymail.com>
2022-03-28 12:46:40 -0500

    installed Apollo and made a schemas folder with a file for type definitions, a file for resolvers and an index.js file

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,9 @@
+/* CREATE TYPE DEFINITIONS TO DEFINE THE DATA THE CLIENT CAN EXPECT TO WORK WITH */
+// Import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// Create our type definitions (typeDefs)
+const typeDefs = gql``;
+
+// Export the typeDefs
+module.exports = typeDefs;
