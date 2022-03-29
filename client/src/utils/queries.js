d00504cf4bbf93ec310e932a0b55c49fca2fc079
Nicholas Nolen <nicnolen@ymail.com>
2022-03-29 11:33:41 -0500

    created a query for thoughts in the queries.js file in the utils folder in the client folder

---
+++ client/src/utils/queries.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_THOUGHTS = gql`
+  query thoughts($username: String) {
+    thoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reaction {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  };
