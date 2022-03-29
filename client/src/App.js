c07a90bbb76036c20550bcdf1918c33bd9e3438e
Nicholas Nolen <nicnolen@ymail.com>
2022-03-29 10:14:59 -0500

    installed and set up Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import {
+  ApolloProvider, // React component that provides data to other components
+  ApolloClient, // Constructor function that helps initialize the GraphQL API server connection
+  InMemoryCache, // Enables Apollo Client to cache API response data
+  createHttpLink, // Control how Apollo Client makes a request
+} from '@apollo/client';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// Establish link to the GraphQL server
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+// Initiate the ApolloClient instance and create connection to the API endpoint
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(), // initiate a new cache
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+        <div className='container'>
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
