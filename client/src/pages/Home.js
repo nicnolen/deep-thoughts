32a803ee5d4817dd3f7251d5c22030836f255b09
Nicholas Nolen <nicnolen@ymail.com>
2022-03-29 13:23:09 -0500

    added thoughts query to the Home component in the client folder

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,17 @@
+//! Import Dependencies
 import React from 'react';
 
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
+
+//! MAKE HOME COMPONENT
 const Home = () => {
+  //* use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS); // Apollo/client library provides a loading property to indicate the request isnt done and when it is finished, the information is stored in the data property
+  //* if data exists, store it in the thoughts constraint. If data is undefined, save an empty array to the thoughts component
+  const thoughts = data?.thoughts || [];
+  console.info(thoughts);
+
   return (
     <main>
       <div className='flex-row justify-space-between'>
--- server/server.js
+++ server/server.js
@@ -1,43 +1,43 @@
-// Import dependencies
+//! Import dependencies
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express'); // Import ApolloServer
 const { authMiddleware } = require('./utils/auth');
 
-// Import personal files
+//! Import personal files
 const { typeDefs, resolvers } = require('./schemas'); // Import typeDefs and resolvers
 const db = require('./config/connection'); // Import Mongoose database connection
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Start a new Apollo server
+//! Start a new Apollo server
 const startServer = async () => {
-  // create a new Apollo server and pass in our schema data
+  //* create a new Apollo server and pass in our schema data
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware
   });
 
-  // start the Apollo server
+  //* start the Apollo server
   await server.start();
 
-  // integrate our Apollo server with the Express application as middleware
+  //* integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
 
-  // log where we can go to test oour GQL API
+  //* log where we can go to test our GQL API
   console.info(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
-// Initialize the Apollo server
+//! Initialize the Apollo server
 startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Listen for connection to be made the first time the database is opened using db.once('open')
+//! Listen for connection to be made the first time the database is opened using db.once('open')
 db.once('open', () => {
-  // upon successful connection, start the server
+  //* upon successful connection, start the server
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
