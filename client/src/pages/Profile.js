55dffd5c923bfd523ed97acb3e97b314158f8bf1
Nicholas Nolen <nicnolen@ymail.com>
2022-03-30 09:41:16 -0500

    added FriendList component and rendered it to the Profile component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
@@ -13,7 +14,7 @@ const Profile = () => {
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(QUERY_USER, {
-    variables: { username: userParam }
+    variables: { username: userParam },
   });
 
   const user = data?.user || {};
@@ -32,7 +33,18 @@ const Profile = () => {
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
-          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+          <ThoughtList
+            thoughts={user.thoughts}
+            title={`${user.username}'s thoughts...`}
+          />
+        </div>
+
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
         </div>
       </div>
     </div>
