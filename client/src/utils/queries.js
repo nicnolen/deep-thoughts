0a659118eb447ee928eb48669b00d8e895c59012
Nicholas Nolen <nicnolen@ymail.com>
2022-03-30 09:02:24 -0500

    created a query to get thoughts by id in queries.js file

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,30 @@
+'CREATE THE THOUGHT AND REACTION QUERIES';
+'Import dependencies';
 import { gql } from '@apollo/client';
 
 export const QUERY_THOUGHTS = gql`
+  #! Get all thoughts
   query thoughts($username: String) {
     thoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_THOUGHT = gql`
+  #! Get thought by id
+  query thoughts($id: ID!) {
+    thought(_id: $id) {
       _id
       thoughtText
       createdAt
