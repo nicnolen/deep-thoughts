9e886e866d2d867feb5d5c4fc7d853ef1e1e67cb
Nicholas Nolen <nicnolen@ymail.com>
2022-03-31 08:52:58 -0500

    added a character count for a new thought

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,16 +1,31 @@
-//TODO: THOUGHT FORM PAGE 
+//TODO: THOUGHT FORM PAGE
 //! Import dependencies
-import React from 'react';
+import React, { useState } from 'react';
 
 //! Import ThoughtForm component
 const ThoughtForm = () => {
+  const [thoughtText, setText] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
+
+  // update state based on form input changes
+  const handleChange = event => {
+    if (event.target.value.length <= 280) {
+      setText(event.target.value);
+      setCharacterCount(event.target.value.length);
+    }
+  };
+
   return (
     <div>
-      <p className="m-0">Character Count: 0/280</p>
+      <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+        Character Count: {characterCount}/280
+      </p>
       <form className="flex-row justify-center justify-space-between-md align-stretch">
         <textarea
           placeholder="Here's a new thought..."
-          className="form-input col-12 col-md-9"></textarea>
+          value={thoughtText}
+          className="form-input col-12 col-md-9"
+          onChange={handleChange}></textarea>
         <button className="btn col-12 col-md-3" type="submit">
           Submit
         </button>
