94369861fa71b88d539bf25decfb08e7aea989bf
Nicholas Nolen <nicnolen@ymail.com>
2022-04-27 10:39:21 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,60 +1,43 @@
-//TODO: BACK END SERVER FILE
-//! Import dependencies
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express'); // Import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-//! Import personal files
-const { typeDefs, resolvers } = require('./schemas'); // Import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection'); // Import Mongoose database connection
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//! Start a new Apollo server
 const startServer = async () => {
-  //* create a new Apollo server and pass in our schema data
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
   });
 
-  //* start the Apollo server
+  // starting the apollo server
   await server.start();
 
-  //* integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
-
-  //* log where we can go to test our GQL API
   console.info(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
-
-//! Initialize the Apollo server
 startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//! Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/public/serviceWorker.js'));
-});
-
-//! Catch-all route where any route that isnt defined is treated as a 404 error
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-//! Listen for connection to be made the first time the database is opened using db.once('open')
 db.once('open', () => {
-  //* upon successful connection, start the server
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    console.info(`API server running on port ${PORT}!`);
   });
 });
