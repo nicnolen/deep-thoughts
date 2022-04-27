e901b9204357346ac2ac3fbd646347bc143200e7
Nicholas Nolen <nicnolen@ymail.com>
2022-04-27 10:18:35 -0500

    fixed the serviceWorker.js file link in server.js

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'service-worker.js'));
+  res.sendFile(path.resolve(__dirname, 'public', 'serviceWorker.js'));
 });
 
 //! Catch-all route where any route that isnt defined is treated as a 404 error
