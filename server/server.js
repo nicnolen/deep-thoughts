481e7278441fc9f165eb229805bb4ad0339d09d3
Nicholas Nolen <nicnolen@ymail.com>
2022-04-27 11:12:48 -0500

    added service worker link to server.js file

--- server/server.js
+++ server/server.js
@@ -32,6 +32,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/ServiceWorker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'ServiceWorker.js'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client'));
 });
